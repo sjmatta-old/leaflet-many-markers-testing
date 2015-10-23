bc72883ba8d23aa6ff956560e94059e7dc2fcfa0
Stephen Matta <stephenjmatta+github@gmail.com>
2015-10-23 11:31:57 -0400

    Naive 10k markers. A bit slow.

--- es6/main.js
+++ es6/main.js
@@ -1,11 +1,20 @@
 /* globals L */
 
-import Leaflet from 'leaflet';
+import L from 'leaflet';
+import Faker from 'Faker';
+import async from 'async';
 import 'leaflet/dist/leaflet.css';
 import './main.css';
 
-const map = Leaflet.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([38.89, -77.09], 4);
 
-Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+L.Icon.Default.imagePath = 'http://api.tiles.mapbox.com/mapbox.js/v1.0.0beta0.0/images';
+
+async.times(10000, () => {
+  const marker = L.marker([Faker.Address.latitude(), Faker.Address.longitude()]);
+  marker.addTo(map);
+});
